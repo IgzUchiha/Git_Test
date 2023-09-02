19b92c35f38d392ea327842811321e5cf0cdf7d8
Igmer Castillo <icastillo@box.com>
2023-09-02 08:37:31 -0700

    first push

---
+++ main.js
@@ -0,0 +1,13 @@
+// pushing git code in  JAVASCRIPT
+
+let man = true
+let woman = true
+
+
+if (man == true) {
+    return "is a man";
+} else if (woman ==  true) {
+    return "is a woman";
+} else {
+    return "hasnt picked"
+}
